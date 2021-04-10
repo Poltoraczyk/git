ee68994b36bbc85705716abbb4b68d9c0ba8a60b
Poltoraczyk <apoltoraczyk@gmail.com>
2021-04-10 14:38:53 +0200

    10.04

--- kurs/13_admin_LTE/AdminLTE/logged/pages/user/content.php
+++ kurs/13_admin_LTE/AdminLTE/logged/pages/user/content.php
@@ -34,11 +34,102 @@
       </div><!-- /.container-fluid -->
     </section>
 
+<?php
+
+    require_once '../scripts/connect.php';
+
+    $sql = "SELECT co.country, c.city, s.skills FROM customers cu JOIN countries co ON cu.nationality_id=co.id JOIN cities c ON cu.cities_id=c.id JOIN skills s ON cu.id=s.customers_id WHERE cu.id=".$_SESSION['logged']['id'];
+    $result = $connect->query($sql);
+    $customer = $result->fetch_assoc();
+
+?>
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
         <div class="row">
           <div class="col-md-3">
+          <div class="card card-primary card-outline">
+              <div class="card-body box-profile">
+                <div class="text-center">
+                  <img class="profile-user-img img-fluid img-circle"
+                       src="../dist/img/user4-128x128.jpg"
+                       alt="User profile picture">
+                </div>
+
+                <h3 class="profile-username text-center">Nina Mcintire</h3>
+
+                <p class="text-muted text-center">Software Engineer</p>
+
+                <ul class="list-group list-group-unbordered mb-3">
+                  <li class="list-group-item">
+                    <b>Followers</b> <a class="float-right">1,322</a>
+                  </li>
+                  <li class="list-group-item">
+                    <b>Following</b> <a class="float-right">543</a>
+                  </li>
+                  <li class="list-group-item">
+                    <b>Friends</b> <a class="float-right">13,287</a>
+                  </li>
+                </ul>
+
+                <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            
+            
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">About Me</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <strong><i class="fas fa-book mr-1"></i> Education</strong>
+
+                <p class="text-muted">
+                  B.S. in Computer Science from the University of Tennessee at Knoxville
+                </p>
+
+                <hr>
+
+                <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
+
+                <p class="text-muted">
+                  <!--Malibu, California-->
+                <?php
+                  echo <<<LOCATION
+                    $customer[country], $customer[city]
+LOCATION;
+                ?>
+                </p>
+
+                <hr>
+                <strong><i class="fas fa-pencil-alt mr-1"></i> Umiejętności</strong>
+
+                <p class="text-muted">
+<?php
+    $sql = "SELECT s.skills FROM customers cu JOIN  skills s ON cu.id=s.customers_id WHERE cu.id=".$_SESSION['logged']['id'];
+    
+    $result = $connect->query($sql);
+    while ($customer = $result->fetch_assoc()) {
+      echo <<<SKILL
+        <span class="tag tag-danger">$customer[skills]</span>
+SKILL;
+    }
+?>
+                
+                </p>
+
+                <hr>
+
+                <strong><i class="far fa-file-alt mr-1"></i> Notes</strong>
+
+                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
+              </div>
+              <!-- /.card-body -->
+            </div>
+
+
             <div class="sticky-top mb-3">
               <div class="card">
                 <div class="card-header">
@@ -92,6 +183,7 @@
             </div>
           </div>
           <!-- /.col -->
+          
           <div class="col-md-9">
             <div class="card card-primary">
               <div class="card-body p-0">
--- kurs/13_admin_LTE/AdminLTE/scripts/login.php
+++ kurs/13_admin_LTE/AdminLTE/scripts/login.php
@@ -24,7 +24,7 @@ session_start();
     */
     //print_r($_POST);
 
-    $sql = "SELECT email, pass, name, surname, permission_id, avatar FROM `customers` WHERE `email`=?";
+    $sql = "SELECT cu.id, cu.email, cu.pass, cu.name, cu.surname, cu.permission_id, cu.avatar FROM `customers` as cu WHERE `email`=?";
     $stmt = $connect->prepare($sql);
     $stmt->bind_param("s", $email);
     
@@ -48,6 +48,7 @@ session_start();
             // echo $customer['email'], $customer['pass'];
             
             if(password_verify($_POST['pass'], $customer['pass'])){
+            $_SESSION['logged']['id'] = $customer['id'];
             $_SESSION['logged']['email'] = $customer['email'];
             $_SESSION['logged']['name'] = $customer['name'];
             $_SESSION['logged']['surname'] = $customer['surname'];
@@ -56,8 +57,27 @@ session_start();
             session_regenerate_id();
             $_SESSION['logged']['session_id'] = session_id();
             //header('location: ../logged/x.php');
-            header('location: ../logged/logged.php');
-            }else{
+            
+            //header('location: ../logged/logged.php'); //dokończyć!?
+            
+            //weryfikacja uprawnień
+            switch ($customer['permission_id']) {
+                case '1':
+                    header('location: ../logged/logged.php');
+                    break;
+
+                case '2':
+                    header('location: ../logged/logged.php');
+                    break;
+                                
+                case '3':
+                    header('location: ../logged/pages/administrator/index2.php');
+                    break;
+                
+            }
+        
+        
+        }else{
                $error = 1;
             }
         }else{
